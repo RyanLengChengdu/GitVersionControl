3273de4a9d8d8a2fb539850fb63b55912ad2fd86
ryanleng <khalillan@hotmail.com>
2021-11-06 23:45:57 +0800

    Initial Commit

---
+++ Shared/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  Shared
+//
+//  Created by 冷锐鹏 on 2021/11/6.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
